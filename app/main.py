b837f702d2de9be75954f23618d9924c45376a0e
Yassine <lamtalaa@gmail.com>
2026-09-15 21:58:33 -0400

    Create CI/CD application

---
+++ app/main.py
@@ -0,0 +1,18 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+VERSION = "1.0.0"
+
+
+@app.get("/")
+def home():
+    return {
+        "application": "CI/CD Homework",
+        "version": VERSION
+    }
+
+
+@app.get("/health")
+def health():
+    return {"status": "healthy"}
